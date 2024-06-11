2e92f8007a55c1daae337b490ebe6ece70d74bf3
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-06-11 19:15:41 +0100

    added supplier model

--- suppliers/models.py
+++ suppliers/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+class Supplier(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=150)
+    email = models.EmailField(blank=True)
+    mobile_number = models.CharField(max_length=14, blank=True)
+    address = models.TextField(blank=True)
+    other_contact_info = models.TextField(blank=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
