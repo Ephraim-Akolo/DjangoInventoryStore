9ccd56d5026e5545ec517f81da5e823a1fb8611d
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-06-11 19:16:44 +0100

    added inventory model

--- inventory/models.py
+++ inventory/models.py
@@ -1,5 +1,17 @@
 from django.db import models
+from suppliers.models import Supplier
 from uuid import uuid4
 
 # Create your models here.
 
+
+class Inventory(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=14, decimal_places=3)
+    suppliers = models.ManyToManyField(Supplier, with_related='items')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
