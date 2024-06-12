6395d34a38392f6d908598d82b1d7abada2a93a1
Jake-Ephraim <ephraimakolo2017@gmail.com>
2024-06-12 06:10:37 +0100

    made email and phone unique

--- suppliers/models.py
+++ suppliers/models.py
@@ -6,8 +6,8 @@ from uuid import uuid4
 class Supplier(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     name = models.CharField(max_length=150)
-    email = models.EmailField(blank=True)
-    mobile_number = models.CharField(max_length=14, blank=True)
+    email = models.EmailField(unique=True)
+    mobile_number = models.CharField(max_length=15, unique=True)
     address = models.TextField(blank=True)
     other_contact_info = models.TextField(blank=True)
     date_joined = models.DateTimeField(auto_now_add=True)
--- suppliers/serializers.py
+++ suppliers/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
+from django.core.validators import RegexValidator
 from .models import Supplier
 
 
 class SupplierSerializer(serializers.ModelSerializer):
-    email = serializers.EmailField(allow_blank=True)
-    mobile_number = serializers.CharField(max_length=14, allow_blank=True)
+    mobile_number = serializers.CharField(max_length=14, validators=[RegexValidator(regex=r'^\+?\d{10,14}$', message="Mobile number must be between 10 and 14 digits.", code='invalid_mobile_number')])
     address = serializers.CharField(allow_blank=True)
     other_contact_info = serializers.CharField(allow_blank=True)
 
